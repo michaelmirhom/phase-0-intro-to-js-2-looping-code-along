b8f36809b769ce1c8759326e36cf1cb6cac2ee5e
michaelmirhom <micharby73@gmail.com>
2023-03-09 23:39:38 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+   
+  }
+ 
+  function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    let i = number;
+  
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
+  
+  
+  
+  
+  
